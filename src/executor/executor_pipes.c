373dd89493dbff84b22808928ea062b15a8961c4
Poukk <alexandrefilho.06@hotmail.com>
2025-09-10 13:17:09 -0300

    feat: add pipe logic to executor

---
+++ src/executor/executor_pipes.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor_pipes.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexanfe <alexanfe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 13:15:55 by alexanfe          #+#    #+#             */
+/*   Updated: 2025/09/10 13:15:56 by alexanfe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+static void	execute_left_command(t_ast_node *node, int pipefd[2])
+{
+	close(pipefd[0]);
+	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+		exit(1);
+	close(pipefd[1]);
+	exit(executor_execute(node));
+}
+
+static void	execute_right_command(t_ast_node *node, int pipefd[2])
+{
+	close(pipefd[1]);
+	if (dup2(pipefd[0], STDIN_FILENO) == -1)
+		exit(1);
+	close(pipefd[0]);
+	exit(executor_execute(node));
+}
+
+static int	wait_for_pipe_children(pid_t left_pid, pid_t right_pid,
+			int pipefd[2])
+{
+	int	left_status;
+	int	right_status;
+
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(left_pid, &left_status, 0);
+	waitpid(right_pid, &right_status, 0);
+	if (WIFEXITED(right_status))
+		return (WEXITSTATUS(right_status));
+	return (1);
+}
+
+int	execute_pipe(t_ast_node *ast)
+{
+	int		pipefd[2];
+	pid_t	left_pid;
+	pid_t	right_pid;
+
+	if (pipe(pipefd) == -1)
+		return (1);
+	left_pid = fork();
+	if (left_pid == -1)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+		return (1);
+	}
+	if (left_pid == 0)
+		execute_left_command(ast->left, pipefd);
+	right_pid = fork();
+	if (right_pid == -1)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+		return (1);
+	}
+	if (right_pid == 0)
+		execute_right_command(ast->right, pipefd);
+	return (wait_for_pipe_children(left_pid, right_pid, pipefd));
+}
--- test/test_executor.c
+++ test/test_executor.c
@@ -70,16 +70,83 @@ Test(executor_tests, test_executor_invalid_command) {
 	gc_free_all(&gc);
 }
 
-Test(executor_tests, test_executor_pipe_not_implemented) {
+Test(executor_tests, test_simple_pipe) {
 	t_gc gc;
-	t_ast_node *node;
+	t_ast_node *pipe_node, *left_cmd, *right_cmd;
+	char *left_args[] = {"echo", "hello", NULL};
+	char *right_args[] = {"cat", NULL};
 	int result;
 	
 	gc_init(&gc);
-	node = ast_node_create(&gc, NODE_PIPE);
 	
-	result = executor_execute(node);
-	cr_assert_eq(result, 1);
+	pipe_node = ast_node_create(&gc, NODE_PIPE);
+	left_cmd = ast_node_create(&gc, NODE_CMD);
+	right_cmd = ast_node_create(&gc, NODE_CMD);
+	
+	ast_node_set_args(&gc, left_cmd, left_args);
+	ast_node_set_args(&gc, right_cmd, right_args);
+	
+	pipe_node->left = left_cmd;
+	pipe_node->right = right_cmd;
+	
+	result = executor_execute(pipe_node);
+	cr_assert_eq(result, 0);
+	
+	gc_free_all(&gc);
+}
+
+Test(executor_tests, test_pipe_with_grep) {
+	t_gc gc;
+	t_ast_node *pipe_node, *left_cmd, *right_cmd;
+	char *left_args[] = {"echo", "hello\nworld\nhello", NULL};
+	char *right_args[] = {"grep", "hello", NULL};
+	int result;
+	
+	gc_init(&gc);
+	
+	pipe_node = ast_node_create(&gc, NODE_PIPE);
+	left_cmd = ast_node_create(&gc, NODE_CMD);
+	right_cmd = ast_node_create(&gc, NODE_CMD);
+	
+	ast_node_set_args(&gc, left_cmd, left_args);
+	ast_node_set_args(&gc, right_cmd, right_args);
+	
+	pipe_node->left = left_cmd;
+	pipe_node->right = right_cmd;
+	
+	result = executor_execute(pipe_node);
+	cr_assert_eq(result, 0);
+	
+	gc_free_all(&gc);
+}
+
+Test(executor_tests, test_multiple_pipes) {
+	t_gc gc;
+	t_ast_node *outer_pipe, *inner_pipe, *cmd1, *cmd2, *cmd3;
+	char *args1[] = {"echo", "test", NULL};
+	char *args2[] = {"cat", NULL};
+	char *args3[] = {"wc", "-l", NULL};
+	int result;
+	
+	gc_init(&gc);
+	
+	outer_pipe = ast_node_create(&gc, NODE_PIPE);
+	inner_pipe = ast_node_create(&gc, NODE_PIPE);
+	cmd1 = ast_node_create(&gc, NODE_CMD);
+	cmd2 = ast_node_create(&gc, NODE_CMD);
+	cmd3 = ast_node_create(&gc, NODE_CMD);
+	
+	ast_node_set_args(&gc, cmd1, args1);
+	ast_node_set_args(&gc, cmd2, args2);
+	ast_node_set_args(&gc, cmd3, args3);
+	
+	inner_pipe->left = cmd1;
+	inner_pipe->right = cmd2;
+	outer_pipe->left = inner_pipe;
+	outer_pipe->right = cmd3;
+	
+	result = executor_execute(outer_pipe);
+	cr_assert_eq(result, 0);
 	
 	gc_free_all(&gc);
 }
